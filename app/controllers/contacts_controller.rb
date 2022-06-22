a9d72f7a9642eb73effaa4acd0419055713339ee
kiiillerbot <dev.shimolkhan@gmail.com>
2022-06-23 01:09:15 +0800

    Create Contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,8 +3,27 @@ class ContactsController < ApplicationController
     if params[:group_id] && !params[:group_id].empty?
       @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
     else
-      @contacts = Contact.page(params[:page])
+      @contacts = Contact.order('created_at DESC').page(params[:page])
     end
     @groups = Group.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Contact created successfully."
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:group_id, :name, :email, :company, :phone, :address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'contacts#index'
   get 'contacts/index', as: 'contacts'
+  get 'contacts/new', as: 'new_contact'
+  post 'contacts/create'
 end
