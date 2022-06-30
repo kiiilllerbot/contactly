32fea844fade7790c6b5a841e0234140addc5ce8
kiiillerbot <dev.shimolkhan@gmail.com>
2022-06-30 15:21:39 +0800

    Implemented Active Record Scope Based Search Feature

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,21 +10,27 @@ class Contact < ApplicationRecord
     validates_presence_of :phone
     validates_presence_of :address
 
-    def self.search(term)
-        if term && !term.empty?
-            where('name ILIKE ?', "%#{term}%")
-        else
-            all
-        end
+    scope :search, -> (term) do
+        where('name ILIKE :term or email ILIKE :term OR company ILIKE :term',term: "%#{term}%")if term.present?
     end
 
-    def self.by_group(group_id)
-        if group_id && !group_id.empty?
-            where(group_id: group_id)
-        else
-            all
-        end
-    end
+    # def self.search(term)
+    #     if term && !term.empty?
+    #         where('name ILIKE ?', "%#{term}%")
+    #     else
+    #         all
+    #     end
+    # end
+
+    scope :by_group, -> (group_id) { where(group_id: group_id) if group_id.present? }
+
+    # def self.by_group(group_id)
+    #     if group_id && !group_id.empty?
+    #         where(group_id: group_id)
+    #     else
+    #         all
+    #     end
+    # end
 
     def gravatar
         hash = Digest::MD5.hexdigest(email.downcase)
