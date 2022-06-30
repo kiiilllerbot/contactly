4087bde7946bb8522ed0ec0be9bdd109a0e8a052
kiiillerbot <dev.shimolkhan@gmail.com>
2022-06-30 15:58:42 +0800

    Final Autocomplete Using jQuery-UI, Jbuilder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,10 @@
 //= require jquery3
 //= require rails-ujs
-//= require jquery-ui
+//= require jquery-ui/core
+//= require jquery-ui/widget
+//= require jquery-ui/position
+//= require jquery-ui/widgets/autocomplete
+//= require jquery-ui/widgets/menu
 //= require activestorage
 //= require turbolinks
 //= require_tree .
@@ -28,6 +32,8 @@ $(function () {
     minLength: 3,
     select: function (event, ui) {
       $("#term").val(ui.item.value);
+      // Click on autocomplete to select the matching suggestion
+      $(this).closest("form").submit();
     },
   });
 });
