79fda0b81ff33863a4cf343dbcfcf5119f013f97
kiiillerbot <dev.shimolkhan@gmail.com>
2022-07-04 01:00:06 +0800

    Customized Validation Errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def has_error?(resource, field)
+        resource.errors.messages[field].present?
+    end
+
+    def get_error(resource, field)
+        "#{field} ".capitalize + resource.errors.messages[field].join(', ')
+    end
 end
